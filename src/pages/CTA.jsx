49b49010d782aafd5bb734dcedb8edf53cdd0628
AT <athinat690@gmail.com>
2023-11-27 19:46:35 +0000

    CTA and footer done

--- src/pages/CTA.jsx
+++ src/pages/CTA.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="m-20 flex flex-col items-center justify-center">
+      <div className="w-[1000px] flex flex-col items-center justify-center">
+        <h1 className="xl_letters">
+          Let&apos;s <span className="xl_letters_gradient">Work</span>
+        </h1>
+        <h1 className="xl_letters flex gap-10">
+          Together
+          <div className="round_green" />
+        </h1>
+        <h1 className="xl_letters">
+          Let&apos;s <span className="xl_letters_blue">Create.</span>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Button } from "../components/index";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mx-[10%] mt-[10%] flex flex-col gap-20">
+      <div className="flex flex-row justify-between items-center">
+        <div className="flex flex-col gap-10">
+          <h2 className="w-[400px] footer_xl_text">
+            Would you like to work with us?{" "}
+            <span className="underline">Let&apos;s! </span> work together
+          </h2>
+          <Button text="Start a Project?" />
+        </div>
+        <div className="flex flex-col gap-4 w-[250px]">
+          <p className="footer_text">
+            Babarsari Sleman, No 23 Ruko Biru Kab. Sleman, Kota Yogyakarta{" "}
+          </p>
+          <div className="flex flex-col gap-1">
+            <p className="footer_text">+62 824 - 2637 - 8823</p>
+            <p className="footer_text">+62 824 - 2637 - 8823</p>
+          </div>
+          <p className="footer_text">Walkagency@studio.io</p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="footer_text">Dribbble</p>
+          <p className="footer_text">Behance</p>
+          <p className="footer_text">LinkedIn</p>
+          <p className="footer_text">Instagram</p>
+          <p className="footer_text">Facebook</p>
+        </div>
+      </div>
+      <div className="mb-12">
+        <p className="copyright_p ">© 2022 Walkagency - All Right Reserved</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
